06bc1f1fc9a39b5c033fb490276d909176f34a76
medha130101 <medha130101@gmail.com>
2023-03-11 23:34:33 +0530

    New Commit

---
+++ CPP-C1/C12.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int number{0};
+    number = 10+20;
+    cout<<"Number is: "<<number<<endl;
+    cout<<"Good Bye!"<<endl;
+    return 0;
+}
